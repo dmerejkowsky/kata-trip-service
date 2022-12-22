e26479a327e8e2183650dca8fc86785c985c72b8
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-12-22 13:03:23 +0100

    Fixup initial code

--- python/trip_service/service.py
+++ python/trip_service/service.py
@@ -1,12 +1,13 @@
 from trip_service.user_session import session
 from trip_service.errors import UserNotLoggedIn
+from trip_service.trip_dao import TripDAO
 
 
 class TripService:
     def get_trips_by_user(self, user):
         trips = []
         is_friend = False
-        logged_user = session.get_logged_user()
+        logged_user = self.get_logged_user()
         if logged_user:
             for friend in user.friends:
                 if friend == logged_user:
